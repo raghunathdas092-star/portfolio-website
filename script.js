6532daf8ca94ea3d179c7beeffbbb4d90b6771ba
RaghunathDas <raghunathdas092@gmail.com>
2026-06-27 17:55:34 +0530

    Day 7: Added animation and premium UI

--- script.js
+++ script.js
@@ -1,1 +1,114 @@
-console.log("Portfolio Website Loaded Successfully!");
+const words = [
+    "Software Developer",
+    "AI Enthusiast",
+    "Cyber Security Expert",
+    "Data Analyst"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const typingElement = document.getElementById("typing-text");
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!deleting) {
+
+        typingElement.textContent =
+            currentWord.substring(0, charIndex++);
+
+        if (charIndex > currentWord.length) {
+            deleting = true;
+
+            setTimeout(typeEffect, 1500);
+
+            return;
+        }
+
+    } else {
+
+        typingElement.textContent =
+            currentWord.substring(0, charIndex--);
+
+        if (charIndex < 0) {
+
+            deleting = false;
+
+            wordIndex =
+                (wordIndex + 1) % words.length;
+
+        }
+
+    }
+
+    setTimeout(typeEffect, deleting ? 60 : 100);
+
+}
+
+typeEffect();
+
+const observer = new IntersectionObserver(entries => {
+
+entries.forEach(entry => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+const hiddenElements = document.querySelectorAll(
+
+".about, .skills, .projects, .experience, .growth, .contact"
+
+);
+
+hiddenElements.forEach(el=>{
+
+el.classList.add("hidden");
+
+observer.observe(el);
+
+});
+
+const sections = document.querySelectorAll("section");
+
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(section=>{
+
+const top=section.offsetTop-150;
+
+if(pageYOffset>=top){
+
+current=section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.getAttribute("href")==="#"+current){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
+
